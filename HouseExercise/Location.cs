a0d9af0b73c3e65bab0037c738f770c0ed97146a
gnivler <gnivil@gmail.com>
2016-06-03 14:35:12 -0600

    more natural wording

--- HouseExercise/Location.cs
+++ HouseExercise/Location.cs
@@ -23,12 +23,12 @@ namespace HouseExercise
         {
             get
             {
-                string description = $"You're standing in the {Name}.  You see exits to the following places: ";
+                string description = $"You're standing in the {Name}.  You can go to the";
                 for (int i = 0; i < Exits.Length; i++)
                 {
                     description += $" {Exits[i].Name}";
                     if (i != Exits.Length -1)
-                        description += ",";
+                        description += " and";
                 }
                 description += ".";
                 return description;
